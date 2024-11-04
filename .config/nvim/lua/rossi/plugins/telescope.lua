cc2d7119c27e81fc70bb07b69a77cb3aee2b2943
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2024-11-04 23:41:39 +0000

    add hidden files on neovim telescope

--- .config/nvim/lua/rossi/plugins/telescope.lua
+++ .config/nvim/lua/rossi/plugins/telescope.lua
@@ -3,11 +3,19 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+		})
+
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Open file search"})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Open file search" })
 		-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set("n", "<leader>fg", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end, { desc = "Open word search"})
+		end, { desc = "Open word search" })
 	end,
 }
