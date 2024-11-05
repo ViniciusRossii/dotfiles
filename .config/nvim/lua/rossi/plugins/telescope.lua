854a57c8529ee0a6a588e72774a833aa70a2e5a9
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2024-11-05 00:46:13 -0300

    add telescope for opened buffers

--- .config/nvim/lua/rossi/plugins/telescope.lua
+++ .config/nvim/lua/rossi/plugins/telescope.lua
@@ -9,10 +9,21 @@ return {
 					hidden = true,
 				},
 			},
+			defaults = {
+				mappings = {
+					i = {
+						["<C-d>"] = require("telescope.actions").delete_buffer,
+					},
+					n = {
+						["<C-d>"] = require("telescope.actions").delete_buffer,
+					},
+				},
+			},
 		})
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Open file search" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Opened buffers" })
 		-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 		vim.keymap.set("n", "<leader>fg", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
