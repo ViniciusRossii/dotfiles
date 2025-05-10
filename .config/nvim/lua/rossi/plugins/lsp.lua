7636f1b487ca8b1a8c22f94f58e7be1c8094d4b4
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2025-05-10 19:16:40 -0300

    remove vue language server

--- .config/nvim/lua/rossi/plugins/lsp.lua
+++ .config/nvim/lua/rossi/plugins/lsp.lua
@@ -30,8 +30,6 @@ return {
 			})
 
 			local mason_registry = require("mason-registry")
-			local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-				.. "/node_modules/@vue/language-server"
 
 			require("mason-lspconfig").setup({
 				ensure_installed = {
@@ -46,15 +44,6 @@ return {
 			})
 
 			require("lspconfig").ts_ls.setup({
-				init_options = {
-					plugins = {
-						{
-							name = "@vue/typescript-plugin",
-							location = vue_language_server_path,
-							languages = { "vue" },
-						},
-					},
-				},
 				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 				require("lspconfig").emmet_ls.setup({
 					capabilities = capabilities,
