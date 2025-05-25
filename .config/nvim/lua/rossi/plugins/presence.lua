c6d832de7daf493b6da3827026c17e750766ce69
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2025-05-25 12:31:20 -0300

    change discord rich presence texts

--- .config/nvim/lua/rossi/plugins/presence.lua
+++ .config/nvim/lua/rossi/plugins/presence.lua
@@ -1,3 +1,9 @@
 return {
-  "andweeb/presence.nvim",
+	"andweeb/presence.nvim",
+	config = function()
+		require("presence").setup({
+			workspace_text = "Working",
+			editing_text = "Coding...",
+		})
+	end,
 }
