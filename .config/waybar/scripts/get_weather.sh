60a4c5920902321bca2fbf24d79b7043c728267b
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2026-05-16 12:25:44 -0300

    update hyprland

--- .config/waybar/scripts/get_weather.sh
+++ .config/waybar/scripts/get_weather.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 for i in {1..5}; do
-  text=$(curl -s "https://wttr.in/$1?format=1")
+  text=$(curl -s --max-time 10 "https://wttr.in/$1?format=1")
   if [[ $? == 0 ]]; then
     text=$(echo "$text" | sed -E "s/\s+/ /g")
-    tooltip=$(curl -s "https://wttr.in/$1?format=4")
+      tooltip=$(curl -s --max-time 10 "https://wttr.in/$1?format=4")
     if [[ $? == 0 ]]; then
       tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
       echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
